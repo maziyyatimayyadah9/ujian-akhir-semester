316715a00e3a9161c3e93885a924193ade615d94
maziyyatimayyadah9 <80758212+maziyyatimayyadah9@users.noreply.github.com>
2021-06-29 09:39:17 +0700

    Update tumpukanApp.java

--- src/main/java/stack_tumpukan/tumpukanApp.java
+++ src/main/java/stack_tumpukan/tumpukanApp.java
@@ -6,11 +6,12 @@ public class tumpukanApp {
     tumpukan.baca();
     tumpukan.push(30);
     tumpukan.baca();
-    long nilai1=tumpukan.pop();
+    long nilai1=tumpukan.peek();
     System.out.println(" nilai teratas = "+nilai1);
     System.out.println(" nama saya adalah Maziyyati Mayyadah ");
     long nilai2 = tumpukan.pop();
     System.out.print(" nilai yang dihapus = "+nilai2);
+    tumpukan.baca();
     System.out.println(" ");
     tumpukan.push(40);
     tumpukan.baca();
